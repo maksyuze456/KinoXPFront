ad4df0a75efad44a3dbc39e3fe0aceba12504098
Maksym <maksym.045z@gmail.com>
2025-03-11 12:42:09 +0100

    all CRUD

--- src/booking/updateBooking.js
+++ src/booking/updateBooking.js
@@ -1,6 +1,10 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookingId = urlParams.get("id");
-fetchBookingById(bookingId);
+if(bookingId != null) {
+    fetchBookingById(bookingId);
+} else {
+    createForm();
+}
 
 async function fetchBookingById(bookingId){
     try {
@@ -19,12 +23,57 @@ function fillForm(booking) {
         <input type="text" id="lastName" name="lastName" value="${booking.lastName}">
         <input type="text" id="phone" name="phone" value="${booking.phone}">
         <input type="number" id="amount" name="amount" value="${booking.amount}">
-        <input type="submit" value="Submit">
+        <input class="submit-button" type="submit" value="Save">
     `;
     form.insertAdjacentHTML("beforeend", markup);
 
     form.addEventListener("submit", handleUpdate);
     
+}
+
+function createForm() {
+    let form = document.querySelector("form");
+    const markup = `
+        <label for="name">First Name:</label>
+        <input type="text" id="name" name="name">
+
+        <label for="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName">
+
+        <label for="phone">Phone:</label>
+        <input type="text" id="phone" name="phone">
+
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" name="amount">
+
+        <input class="submit-button" type="submit" value="Create">
+    `;
+    form.insertAdjacentHTML("beforeend", markup);
+    form.addEventListener("submit", handleCreate)
+}
+
+async function handleCreate(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const lastName = document.getElementById("lastName").value;
+    const phone = document.getElementById("phone").value;
+    const amount = parseInt(document.getElementById("amount").value);
+
+    const booking = { name, lastName, phone, amount };
+    try {
+        let response = await fetch(`http://localhost:8080/booking/create`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(booking)
+        });
+        let createdBooking = await response.json();
+        console.log(createdBooking);
+        window.location.href = `/src/booking/bookings.html`;
+    } catch(err) {
+        console.log("Error: " + err)
+    }
 }
 
 async function handleUpdate(event) {
@@ -45,6 +94,7 @@ async function handleUpdate(event) {
         });
         let updatedBooking = await response.json();
         console.log(updatedBooking);
+        window.location.href = `/src/booking/bookings.html`;
     } catch(err) {
         console.log("Error: " + err);
     }
